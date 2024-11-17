e4996a69a06e2b4d6f271dae4731ca41e149baf7
Vladimir Filatov <thelucky16@Wowas-Macbook.local>
2024-11-17 15:57:23 +0000

    tweaks

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 chromedriver_autoinstaller.install()
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920,1080")
